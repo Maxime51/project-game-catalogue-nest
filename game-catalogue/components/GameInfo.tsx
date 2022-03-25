2dcc1a79f383e863672d605089f3c8d3d387b411
mansuy maxime <mmansuy@norauto.fr>
2022-03-25 14:19:19 +0100

    maj

--- game-catalogue/components/GameInfo.tsx
+++ game-catalogue/components/GameInfo.tsx
@@ -6,8 +6,8 @@ export default function GameInfo(props) {
   const { user, error, isLoading } = useUser();
   const gamesJson = JSON.parse(props.game);
   console.log(user)
-  return <Layout>
-
+  if (user === undefined) {
+    return <Layout>
     <section className="py-5">
   <div className="container px-4 px-lg-5 my-5">
         <div className="row gx-4 gx-lg-5 align-items-center">
@@ -20,17 +20,37 @@ export default function GameInfo(props) {
           </div>
           <p className="lead">{gamesJson.summary}</p>
           <p>Price :{gamesJson.price} €</p><br></br>
-          <div className="d-flex">
-                <a href={`/api/panier/add/${gamesJson._id}?info=${user?.email}`}>
-                <button type="button" className="btn btn-lg btn-danger" data-bs-toggle="popover" title="Add to basket" data-bs-content="add to Basket with success!">
-                  Add to Basket
-                </button>
-                </a>
-
-          </div>
       </div>
     </div>
   </div>
   </section>
   </Layout>
+  } else {
+    return <Layout>
+      <section className="py-5">
+        <div className="container px-4 px-lg-5 my-5">
+          <div className="row gx-4 gx-lg-5 align-items-center">
+            <div className="col-md-6">
+              {gamesJson?.cover?.url ? <img className="card-img-top mb-5 mb-md-0" src={gamesJson.cover.url} alt="..." /> : <img className="card-img-top mb-5 mb-md-0" src={gamesJson.cover.url} alt="..." />}
+            </div>
+            <div className="col-md-6">
+              <h1 className="display-5 fw-bolder">{gamesJson.name}</h1>
+              <div className="fs-5 mb-5">
+              </div>
+              <p className="lead">{gamesJson.summary}</p>
+              <p>Price :{gamesJson.price} €</p><br></br>
+              <div className="d-flex">
+                <a href={`/api/panier/add/${gamesJson._id}?info=${user?.email}`}>
+                  <button type="button" className="btn btn-lg btn-danger" data-bs-toggle="popover" title="Add to basket" data-bs-content="add to Basket with success!">
+                    Add to Basket
+                  </button>
+                </a>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  }
 }
--- game-catalogue/pages/api/panier/add/[game]/index.ts
+++ game-catalogue/pages/api/panier/add/[game]/index.ts
@@ -13,87 +13,92 @@ export default async function handler(
     const idGame = req.query.game;
     const cookie = getCookie("appSession", { req, res });
     const session = getSession(req, res);
-
+    console.log(session);
     const { accessToken } = await getAccessToken(req, res);
-
-    const response = await fetch(
-      `https://${process.env.AUTH0_DOMAIN}/userinfo`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    const data = await response.json();
-
-    const mongodb = await getDatabase();
-    //get user with email
-    const idUser = await mongodb
-      .db()
-      .collection("users")
-      .findOne({ email: session.user.email })
-      .then((result) => result._id);
-
-    //get user with email
-
-    const gameExist = await mongodb
-      .db()
-      .collection("panier")
-      .findOne({
-        users: new ObjectId(idUser),
-        "content.article": new ObjectId(idGame.toString()),
-      })
-      .then((result) => {
-        return result?.content?.map((element) => {
-          if (
-            element.article.toString() ===
-            new ObjectId(idGame.toString()).toString()
-          ) {
-            return element.quantity;
-          }
-        });
-      });
-
-    if (gameExist === undefined) {
-      const addGame = await mongodb
-        .db()
-        .collection("panier")
-        .updateOne(
-          {
-            users: new ObjectId(idUser),
+    if (session) {
+      const response = await fetch(
+        `https://${process.env.AUTH0_DOMAIN}/userinfo`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
           },
-          {
-            $push: {
-              content: {
-                article: new ObjectId(idGame.toString()),
-                quantity: 1,
-              },
-            },
-          }
-        );
-    } else {
-      const gameExitFormat = gameExist.filter(
-        (element) => element !== undefined
+        }
       );
-      const addGame = await mongodb
+      const data = await response.json();
+      console.log("ici");
+      const mongodb = await getDatabase();
+      //get user with email
+      const idUser = await mongodb
+        .db()
+        .collection("users")
+        .findOne({ email: session.user.email })
+        .then((result) => result._id);
+
+      //get user with email
+
+      const gameExist = await mongodb
         .db()
         .collection("panier")
-        .updateOne(
-          {
-            users: new ObjectId(idUser),
-            "content.article": new ObjectId(idGame.toString()),
-          },
-          {
-            $set: {
-              "content.$.quantity": gameExitFormat[0] + 1,
-            },
-          }
-        );
-    }
+        .findOne({
+          users: new ObjectId(idUser),
+          "content.article": new ObjectId(idGame.toString()),
+        })
+        .then((result) => {
+          return result?.content?.map((element) => {
+            if (
+              element.article.toString() ===
+              new ObjectId(idGame.toString()).toString()
+            ) {
+              return element.quantity;
+            }
+          });
+        });
 
-    res.setHeader("Content-Type", "application/json");
-    res.redirect("/panier");
-    res.end(JSON.stringify({ cookie: "cookie" }));
+      if (gameExist === undefined) {
+        const addGame = await mongodb
+          .db()
+          .collection("panier")
+          .updateOne(
+            {
+              users: new ObjectId(idUser),
+            },
+            {
+              $push: {
+                content: {
+                  article: new ObjectId(idGame.toString()),
+                  quantity: 1,
+                },
+              },
+            }
+          );
+      } else {
+        const gameExitFormat = gameExist.filter(
+          (element) => element !== undefined
+        );
+        const addGame = await mongodb
+          .db()
+          .collection("panier")
+          .updateOne(
+            {
+              users: new ObjectId(idUser),
+              "content.article": new ObjectId(idGame.toString()),
+            },
+            {
+              $set: {
+                "content.$.quantity": gameExitFormat[0] + 1,
+              },
+            }
+          );
+      }
+      res.setHeader("Content-Type", "application/json");
+      res.redirect("/panier");
+      res.end(JSON.stringify({ cookie: "cookie" }));
+    } else {
+      console.log("paslog");
+      res.setHeader("Content-Type", "application/json");
+      res.redirect("/");
+      res.end(JSON.stringify({ cookie: "cookie" }));
+    }
   } else {
     res.statusCode = 405;
     res.end();
